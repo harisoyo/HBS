345fb9a29b76053c73142aa328d01f3942a9fe66
pulkitdhingraoyo <pulkit.dhingra@oyorooms.com>
2021-02-17 11:40:39 +0530

    defined user controller

--- src/main/java/com/example/hbs/HotelBookingSystemApplication.java
+++ src/main/java/com/example/hbs/HotelBookingSystemApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HotelBookingSystemApplication {
+public class HotelBookingSystemApplication
+{
 
-	public static void main(String[] args) {
-		SpringApplication.run(HotelBookingSystemApplication.class, args);
+	public static void main(String[] arg)
+	{
+		//SpringApplication.run(HotelBookingSystemApplication.class, args);
 	}
 
 }
