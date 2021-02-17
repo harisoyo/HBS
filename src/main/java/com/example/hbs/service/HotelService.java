ca5c7f85f0fc6505b6e64dfece0bc22c478049e0
aryan-dua <aryan.dua@oyorooms.com>
2021-02-17 19:43:39 +0530

    Added Hotel DTOs

--- src/main/java/com/example/hbs/service/HotelService.java
+++ src/main/java/com/example/hbs/service/HotelService.java
@@ -1,5 +1,6 @@
 package com.example.hbs.service;
 
+import com.example.hbs.dto.HotelResponseDto;
 import com.example.hbs.enums.Role;
 import com.example.hbs.model.Hotel;
 import com.example.hbs.model.User;
@@ -11,14 +12,28 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class HotelService {
     @Autowired
     private HotelRepository hotelRepository;
 
-    public Page<Hotel> findAllHotels() {
-        Pageable firstPage = PageRequest.of(0, 20);
-        return hotelRepository.findAll(firstPage);
+    public List<HotelResponseDto> findAllHotels(int pageNo) {
+        Pageable firstPage = PageRequest.of(pageNo-1, 20);
+        Page<Hotel> hotels = hotelRepository.findAll(firstPage);
+        List<HotelResponseDto> hotelResponseDtos = new ArrayList<>();
+        for(int iterateHotel = 0;iterateHotel<20;iterateHotel++){
+            Hotel hotel = hotels.getContent().get(iterateHotel);
+            HotelResponseDto responseDto = HotelResponseDto.builder().
+                    hotelName(hotel.getHotelName()).
+                    hotelLocation(hotel.getHotelLocation()).
+                    availableRooms(hotel.getAvailableRooms()).
+                    priceOfRoom(hotel.getPriceOfRoom()).build();
+            hotelResponseDtos.add(responseDto);
+        }
+        return hotelResponseDtos;
     }
 
     public ResponseEntity<String> addHotel(Hotel hotel) {
@@ -44,13 +59,15 @@ public class HotelService {
         }
     }
 
-    public ResponseEntity<String> updateHotel(Long id, Hotel hotel) {
+    public ResponseEntity<String> updateHotel(Long id, HotelResponseDto hotelResponseDto) {
         if (hotelRepository.findById(id).isPresent()) {
             User user = hotelRepository.findById(id).get().getUser();
             if (user.getUserRole().equals(Role.HOTEL_OWNER)) {
-                hotelRepository.deleteById(id);
+                Hotel hotel = hotelRepository.getOne(id);
+                hotel.setAvailableRooms(hotelResponseDto.getAvailableRooms());
+                hotel.setPriceOfRoom(hotelResponseDto.getPriceOfRoom());
                 hotelRepository.save(hotel);
-                return ResponseEntity.ok("Hotel Deleted");
+                return ResponseEntity.ok("Hotel Updated");
             } else {
                 return ResponseEntity.badRequest().body("Customers are not allowed to update a Hotel");
             }
