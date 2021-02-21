b41243a976b40e4bd848a4d41263439c7cfe8b58
aryan-dua <aryan.dua@oyorooms.com>
2021-02-21 12:22:27 +0530

    All Required Apis and Controllers  Added

--- src/main/java/com/example/hbs/service/BookingService.java
+++ src/main/java/com/example/hbs/service/BookingService.java
@@ -2,8 +2,11 @@ package com.example.hbs.service;
 
 import com.example.hbs.dto.BookingRequestDto;
 import com.example.hbs.dto.BookingResponseDto;
+import com.example.hbs.enums.Role;
 import com.example.hbs.model.Booking;
 import com.example.hbs.repository.BookingRepository;
+import com.example.hbs.repository.HotelRepository;
+import com.example.hbs.repository.UserRepository;
 import com.example.hbs.service.mapper.BookingMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,15 +17,30 @@ public class BookingService {
     BookingRepository bookingRepository;
 
     @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    HotelRepository hotelRepository;
+
+    @Autowired
     BookingMapper bookingMapper;
 
     public BookingResponseDto addBooking(BookingRequestDto bookingRequestDto) {
-        Booking booking = Booking.builder()
-                .bookingPrice(bookingRequestDto.getBookingPrice())
-                .noOfRooms(bookingRequestDto.getNoOfRooms())
-                .noOfPeople(bookingRequestDto.getNoOfPeople())
-                .build();
-        return bookingMapper.map(bookingRepository.save(booking));
+        if (userRepository.findById(bookingRequestDto.getUserId()).isPresent() &&
+                userRepository.findById(bookingRequestDto.getUserId()).get().getUserRole().equals(Role.HOTEL_OWNER) &&
+                hotelRepository.findById(bookingRequestDto.getHotelId()).isPresent()) {
+            Booking booking = Booking.builder()
+                    .user(userRepository.findById(bookingRequestDto.getUserId()).get())
+                    .hotel(hotelRepository.findById(bookingRequestDto.getHotelId()).get())
+                    .bookingPrice(bookingRequestDto.getBookingPrice())
+                    .noOfRooms(bookingRequestDto.getNoOfRooms())
+                    .noOfPeople(bookingRequestDto.getNoOfPeople())
+                    .build();
+            return bookingMapper.map(bookingRepository.save(booking));
+        }
+        else{
+            return null;
+        }
     }
 
     public BookingResponseDto deleteBooking(Long id) {
